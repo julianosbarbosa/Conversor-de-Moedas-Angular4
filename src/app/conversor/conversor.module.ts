1a502dbf8ea1937ef2aad6ba744035944523219d
Juliano <juliano@nationphone.com>
2017-10-14 20:03:29 -0300

    UPDATE - estrutura conversor de moedas

---
+++ src/app/conversor/conversor.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ConversorComponent } from './conversor/conversor.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ConversorComponent]
+})
+export class ConversorModule { }
---
+++ src/app/conversor/models/conversao-response.model.ts
@@ -0,0 +1,6 @@
+export class ConversorResponse {
+  constructor(
+    public base: string,
+    public date: string,
+    public rates: any){}
+}
